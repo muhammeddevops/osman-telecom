5a6214422cb3c718d1dbebf4a569804da27529b4
Bilaal Ahmad <bilaal-ahmad@hotmail.co.uk>
2023-08-25 21:44:43 +0100

    feat: Integrated react-hook-form into RegisterForm with input validation

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,30 +1,33 @@
-"use client";
+'use client';
 
-import { useState } from "react";
+import { useForm } from 'react-hook-form';
+import validateForm from '@/utils/validate-form';
+
+/** TODO input validation
+ * email -> ensure valid email is entered ✅
+  -> To truly validate email, send an email to the user to validate their account using something like SendGrid ❌
+  -> what do you do if the email is not validated after registration? delete the account? ❌
+ * password -> minLength of [x], contains at least 1 uppercase letter, 1 lowercase letter, 1 number & 1 special character ✅
+ * confirmPassword -> matches password field ✅
+ */
 
 export default function RegisterForm() {
-  const [form, setForm] = useState({
-    email: "",
-    password: "",
-    confirmPassword: "",
-  });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting, touchedFields },
+    trigger,
+  } = useForm({ mode: 'onTouched' });
+  // mode: "onTouched" - validates initially onBlur, subsequently onChanged
+  // Same behaviour as default "onSubmit" but validates without having to submit
 
-  console.log(form);
+  //! TESTING ONLY
+  if (Object.keys(errors).length) console.log('errors:', errors);
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
+  const onSubmit = async (data) => {
+    console.log('input values:', data);
 
-    setForm({ ...form, [name]: value });
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    if (form.password !== form.confirmPassword) {
-      console.log("passwords dont match!");
-      return;
-    }
-
-    try {
+    /* try {
       const res = await fetch("/api/auth/register", {
         method: "POST",
         headers: {
@@ -40,14 +43,17 @@ export default function RegisterForm() {
     } catch (err) {
       //   setError(err);
       //   console.log(err);
-    }
+    } */
   };
+
   return (
     <div className="flex flex-col justify-center items-center gap-10 border border-slate-950 p-8 rounded-md">
       <h1 className="text-center text-4xl">Sign up</h1>
       <form
-        onSubmit={handleSubmit}
+        onSubmit={handleSubmit(onSubmit)}
         className="flex flex-col gap-8 justify-center items-center w-96"
+        noValidate
+        method="POST"
       >
         <div className="form-group">
           <label htmlFor="email" className="form-label">
@@ -56,54 +62,86 @@ export default function RegisterForm() {
 
           <input
             id="email"
-            type="text"
-            name="email"
-            placeholder="Enter your email..."
-            required
             className="form-input"
-            onChange={handleChange}
+            type="text"
+            placeholder="Enter your email..."
+            {...register('email', {
+              required: validateForm.required('email'),
+              validate: (value) => validateForm.email(value),
+            })}
           />
+
+          {errors.email?.message && (
+            <p className="text-red-600 font-semibold">
+              * {errors.email.message}
+            </p>
+          )}
         </div>
 
         <div className="form-group">
-          <label
-            htmlFor="password"
-            className="form-label"
-            onChange={handleChange}
-          >
+          <label htmlFor="password" className="form-label">
             Password
           </label>
 
           <input
             id="password"
-            type="password"
-            name="password"
-            placeholder="Enter your password..."
-            required
             className="form-input"
-            onChange={handleChange}
+            type="password"
+            placeholder="Enter your password..."
+            {...register('password', {
+              required: validateForm.required('password'),
+              validate: (value) => validateForm.registerPassword(value),
+              onBlur: () => {
+                // Manually revalidate confirmPassword when the field has been touched & its error type is not 'required'
+                if (
+                  touchedFields.confirmPassword &&
+                  errors.confirmPassword?.type !== 'required'
+                ) {
+                  trigger('confirmPassword');
+                }
+              },
+            })}
           />
+
+          {errors.password?.message && (
+            <p className="text-red-600 font-semibold">
+              * {errors.password.message}
+            </p>
+          )}
         </div>
 
         <div className="form-group">
           <label htmlFor="confirm-password" className="form-label">
-            Confirm password
+            Confirm Password
           </label>
 
           <input
             id="confirm-password"
-            type="password"
-            name="confirmPassword"
-            placeholder="Confirm your password..."
-            required
             className="form-input"
-            onChange={handleChange}
+            type="password"
+            placeholder="Confirm your password..."
+            {...register('confirmPassword', {
+              required: validateForm.required(),
+              validate: (_, values) => {
+                return validateForm.confirmPasswordsMatch(
+                  values.password,
+                  values.confirmPassword
+                );
+              },
+            })}
           />
+
+          {errors.confirmPassword?.message && (
+            <p className="text-red-600 font-semibold">
+              * {errors.confirmPassword.message}
+            </p>
+          )}
         </div>
 
         <button
           type="submit"
-          className="font-bold uppercase bg-red-600 text-white px-8 py-4 rounded-md"
+          className="font-bold uppercase bg-red-600 text-white px-8 py-4 rounded-md disabled:bg-slate-700"
+          disabled={isSubmitting}
         >
           Register
         </button>
---
+++ src/utils/validate-form.js
@@ -0,0 +1,68 @@
+import { validate as validateEmail } from 'email-validator';
+
+const validateForm = {
+  // Returns the error message to display for a required field
+  required(inputName) {
+    if (!inputName) return 'This is a required field';
+
+    const formattedInputName =
+      inputName[0].toUpperCase() + inputName.slice(1).toLowerCase();
+    return `${formattedInputName} is required`;
+  },
+
+  /** react-hook-form (RHF) validate methods:
+   * valid --> if true is returned
+   * invalid --> if false or string error message is returned
+   
+   * When invalid, RHF returns an error for the corresponding input
+   * Then we can use the errors to render error messages per input in the UI 
+   */
+  email(email, message = 'Incorrect email format') {
+    return validateEmail(email) || message;
+  },
+
+  registerPassword(pw) {
+    // Validate length is between 8-16 (inclusive)
+    if (pw.length < 8) return 'Must contain at least 8 characters';
+    if (pw.length > 16) return 'Must not exceed length of 16 characters';
+
+    // Contains correct chars, one of each: uppercase, lowercase, number, special
+    const hasUppercaseLetter = /[A-Z]/g.test(pw);
+    const hasLowercaseLetter = /[a-z]/g.test(pw);
+    const hasNumber = /[0-9]/g.test(pw);
+    const hasSpecialCharacter = /[^A-Za-z0-9\s]/g.test(pw);
+
+    // Valid password
+    if (
+      hasUppercaseLetter &&
+      hasLowercaseLetter &&
+      hasNumber &&
+      hasSpecialCharacter
+    ) {
+      return true;
+    }
+
+    // Invalid password, return prompt to help user create strong password
+    let amendmentsRequired = [];
+
+    if (!hasUppercaseLetter) amendmentsRequired.push('an uppercase letter');
+    if (!hasLowercaseLetter) amendmentsRequired.push('a lowercase letter');
+    if (!hasNumber) amendmentsRequired.push('a number');
+    if (!hasSpecialCharacter)
+      amendmentsRequired.push(
+        'a special character: ()`~!@#$%^&*-+=|{}[]:;"\'<>,.?/_'
+      );
+
+    return (
+      `Missing required character${
+        amendmentsRequired.length > 1 ? 's' : ''
+      }: ` + amendmentsRequired.join(', ')
+    );
+  },
+
+  confirmPasswordsMatch(password, confirmPassword) {
+    return password === confirmPassword || "Passwords don't match";
+  },
+};
+
+export default validateForm;
