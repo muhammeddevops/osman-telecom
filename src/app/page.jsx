0034fd1f705e9c7bc20e82c3563cb77c89e3f14d
Muhammed <muhammednabeel@live.co.uk>
2023-10-19 15:41:20 +0100

    chore: mantine ui setup

---
+++ src/app/page.jsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import { Button } from "@mantine/core";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-black">
+      <p className="text-xl font-extrabold text-red-600 ml-11">
+        on page.js 123
+      </p>
+      <Button component={Link} href="/dashboard/products">
+        Dashboard
+      </Button>
+    </main>
+  );
+}
