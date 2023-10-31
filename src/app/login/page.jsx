4e7503efb16a110df1b5ad69b52940342596a1a3
Bilaal Ahmad <bilaal-ahmad@hotmail.co.uk>
2023-10-31 14:56:54 +0000

    feat: prevent access to /login & /register for authenticated users

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,6 +1,20 @@
+import { getServerSession } from 'next-auth';
+import { authOptions } from '../api/auth/[...nextauth]/route';
+import { redirect } from 'next/navigation';
+
 import LoginForm from '@/components/LoginForm';
 
-export default function LoginPage() {
+export default async function LoginPage() {
+  const session = await getServerSession(authOptions);
+
+  if (session) {
+    if (session.user.role === 'admin') {
+      redirect('/dashboard');
+    } else {
+      redirect('/');
+    }
+  }
+
   return (
     <main className="form-container">
       <LoginForm />
--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -1,6 +1,20 @@
+import { getServerSession } from 'next-auth';
+import { authOptions } from '../api/auth/[...nextauth]/route';
+import { redirect } from 'next/navigation';
+
 import RegisterForm from '@/components/RegisterForm';
 
-export default function RegisterPage() {
+export default async function RegisterPage() {
+  const session = await getServerSession(authOptions);
+
+  if (session) {
+    if (session.user.role === 'admin') {
+      redirect('/dashboard');
+    } else {
+      redirect('/');
+    }
+  }
+
   return (
     <main className="form-container">
       <RegisterForm />
